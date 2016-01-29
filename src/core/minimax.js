23ea21fc140e18117469cc9b1a3458a5467e774b
Brandon Zeeb <phasebash@gmail.com>
2016-01-28 21:47:51 -0500

    contract validation of constructor arguments.

--- src/core/minimax.js
+++ src/core/minimax.js
@@ -2,7 +2,11 @@
 
 class Minimax {
 
-  constructor() {
+  constructor(actionFn, transitionFn, terminationFn, utilityFn) {
+    this._actionFn      = actionFn;
+    this._transitionFn  = transitionFn;
+    this._terminationFn = terminationFn;
+    this._utilityFn     = utilityFn;
   }
 
   decide(/*state*/) {
--- test/core/minimax-test.js
+++ test/core/minimax-test.js
@@ -21,4 +21,37 @@ describe('minimax', () => {
 
   });
 
+  describe('construction', () => {
+    
+    var f;
+
+    beforeEach(() => {
+      f = function() {}; 
+    });
+
+    it('should provide action function as first argument', () => {
+      const m = new Minimax(f);
+      
+      assert.equal(m._actionFn, f); 
+    });
+
+    it('should provide transition function as second argument', () => {
+      const m = new Minimax(null, f);
+      
+      assert.equal(m._transitionFn, f); 
+    });
+
+    it('should provide termination function as third argument', () => {
+      const m = new Minimax(null, null, f);
+      
+      assert.equal(m._terminationFn, f); 
+    });
+
+    it('should provide utility function as fourth argument', () => {
+      const m = new Minimax(null, null, null, f);
+      
+      assert.equal(m._utilityFn, f); 
+    });
+  });
+
 });
