a784c2b9861752271beda3e3c63e2bdd6387d629
Brandon Zeeb <zeeb.brandon@gmail.com>
2017-10-29 14:04:19 -0400

    shell of search.

---
+++ src/core/search.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class Search {
+
+  constructor(/*generator*/) {
+  }
+
+  search(/*fn*/) {
+    return {};
+  }
+
+  static dfs() {
+    return new Search();
+  }
+}
+
+module.exports = Search;
+
---
+++ test/core/search-test.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const Search = require('../../src/core/search');
+const assert  = require('chai').assert;
+
+describe('search', () => {
+
+  describe('function contracts', () => {
+
+    it('should export function from module declaration', () => {
+      assert.typeOf(Search, 'function');
+    });
+
+    it('should provide a search function', () => {
+      assert.typeOf(new Search().search, 'function');
+    });
+
+    it('should provide a static dfs function', () => {
+      assert.typeOf(Search.dfs, 'function');
+    });
+
+  });
+
+});
